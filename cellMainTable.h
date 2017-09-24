ea5c62ed36467701c78a13c1757e357cb869a29f
Sergio Martinez <sergiomartinez@Sergios-Mac.local>
2017-09-23 20:29:38 -0500

    Cell objects were connected to m file

---
+++ cellMainTable.h
@@ -0,0 +1,16 @@
+//
+//  cellMainTable.h
+//  Mi Primera Tabla
+//
+//  Created by Sergio Martinez on 9/23/17.
+//  Copyright © 2017 Sergio Martinez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cellMainTable : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imgUser;
+@property (weak, nonatomic) IBOutlet UILabel *lbName;
+@property (weak, nonatomic) IBOutlet UILabel *lbAge;
+
+@end
